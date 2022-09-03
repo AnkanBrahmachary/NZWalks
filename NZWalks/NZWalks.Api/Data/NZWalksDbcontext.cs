b5f37c58f00a8822ae09aa40aaa4745da9cad77b
AnkanBrahmachary <Ankan.Brahmachary@gds.ey.com>
2022-09-03 17:48:08 +0530

    Section @ end

---
+++ NZWalks/NZWalks.Api/Data/NZWalksDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Models.Domains;
+
+namespace NZWalks.Api.Data
+{
+    public class NZWalksDbcontext: DbContext
+    {
+        public NZWalksDbcontext(DbContextOptions<NZWalksDbcontext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
