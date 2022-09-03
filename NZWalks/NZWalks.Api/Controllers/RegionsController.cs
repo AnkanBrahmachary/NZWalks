d45383fbca21c9745fde21557fbbfd4d99b2e0df
AnkanBrahmachary <Ankan.Brahmachary@gds.ey.com>
2022-09-03 18:34:15 +0530

    SEC2

---
+++ NZWalks/NZWalks.Api/Controllers/RegionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Models.Domains;
+
+namespace NZWalks.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>() 
+            { 
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Wellington",
+                    Code="WEL",
+                    Latitude=-23987,
+                    Longitude=-767665,
+                    Area=235 , 
+                    Population=767676
+                },
+
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Delhi",
+                    Code="DEL",
+                    Latitude=-23987,
+                    Longitude=-8787,
+                    Area=9898,
+                    Population=65765765
+                    
+                }
+                 
+
+            };
+            return Ok(regions);
+        }
+    }
+}
